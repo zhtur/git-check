39bd2d7e6033192b06001ed608119f2368bb0082
Zhanibek Turebayev <studyzhtur@gmail.com>
2022-08-30 22:28:10 +0500

    third branch added and modified script file

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 document.body.innerHTML = '<h1>hello from JavaScrit 3!!!!!!!!!!!!!!!!!!!!!!!1</h1>'
-document.body.innerHTML = '<h1>New Branch here for updated and merge</h1>'
+document.body.innerHTML = '<h1>New Branch here for updated and merge - updated from third branch</h1>'
+document.body.innerHTML = '<h1>Third branch and new line</h1>'
